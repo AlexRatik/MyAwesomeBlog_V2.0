dc5684d529e85d24575e8ed0469d3d5406eaef12
Ratik <aleksandr-ratkevich@mail.ru>
2021-02-06 03:40:48 +0300

    Finaly version

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 
-	path('', views.showblog, name = 'showblog' )
+	path('', views.showblog, name = 'showblog' ),
+	path('<int:post_id>/', views.ind_post, name = 'ind_post'),
 
 ]
